496a659cc89db1455d3df928e0b7db100788f3c5
Mattias Fredriksson <mattias-fredriksson91@hotmail.se>
2018-02-21 15:06:52 +0100

    Re-ordered externals added vulkan

--- VulkanProject/VulkanProject/main.cpp
+++ VulkanProject/VulkanProject/main.cpp
@@ -1,6 +1,6 @@
 #define VK_USE_PLATFORM_WIN32_KHR	// required for windows-specific vulkan structs and functions
 #include "vulkan\vulkan.h"
-#include "glm.hpp"
+#include "glm\glm.hpp"
 
 int main(int argc, const char* argv)
 {
