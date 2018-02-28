def445a03d202b5bece90ab7532230b38d59d5f4
Mattias Fredriksson <mattias-fredriksson91@hotmail.se>
2018-02-28 19:30:20 +0100

    Added time tracker

--- VulkanProject/VulkanProject/main.cpp
+++ VulkanProject/VulkanProject/main.cpp
@@ -6,6 +6,9 @@
 #include "Scenes/ComputeScene.h"
 #undef main
 
+void updateWinTitle(VulkanRenderer *rend);
+
+
 int main(int argc, const char* argv)
 {
 	VulkanRenderer renderer;
@@ -20,8 +23,38 @@ int main(int argc, const char* argv)
 			if (windowEvent.type == SDL_KEYUP && windowEvent.key.keysym.sym == SDLK_ESCAPE) break;
 		}
 		renderer.frame();
+		updateWinTitle(&renderer);
 	}
 	
 	renderer.beginShutdown();
 	renderer.shutdown();
 }
+
+
+
+
+void updateWinTitle(VulkanRenderer *rend)
+{
+#define WINDOW_SIZE 10
+	static Uint64 start = 0;
+	static Uint64 last = 0;
+	static double avg[WINDOW_SIZE] = { 0.0 };
+	static double lastSum = 10.0;
+	static int loop = 0;
+	static char gTitleBuff[256];
+	static double gLastDelta = 0.0;
+
+	last = start;
+	start = SDL_GetPerformanceCounter();
+	double deltaTime = (double)((start - last) * 1000.0 / SDL_GetPerformanceFrequency());
+	// moving average window of WINDOWS_SIZE
+	lastSum -= avg[loop];
+	lastSum += deltaTime;
+	avg[loop] = deltaTime;
+	loop = (loop + 1) % WINDOW_SIZE;
+	gLastDelta = (lastSum / WINDOW_SIZE);
+
+	// Set title
+	sprintf_s(gTitleBuff, 256, "%3.0lf", gLastDelta);
+	rend->setWinTitle(gTitleBuff);
+};
